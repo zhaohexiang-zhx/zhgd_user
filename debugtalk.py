4288832879f3e5c127c6f9fad3e9f10455e7be23
zhaohexiang-zhx <zhx190523@163.com>
2020-08-06 10:37:58 +0800

    将参数变量化

--- debugtalk.py
+++ debugtalk.py
@@ -3,3 +3,11 @@ import time
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+#11位
+def get_timestamp():
+    return str(int(time.time() * 10))  
+
+#5位
+def get_timestamp1():
+    return str(time.time()).replace(".", "")[5:10]
