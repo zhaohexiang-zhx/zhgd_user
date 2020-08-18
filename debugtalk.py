37900c1bcd70527c7a59429e355f0c818d33405d
zhaohexiang-zhx <zhx190523@163.com>
2020-08-18 15:55:43 +0800

    新建ceshi_admin所属的三级客户

--- debugtalk.py
+++ debugtalk.py
@@ -14,7 +14,7 @@ def get_timestamp():
 def get_timestamp1():
     return str(time.time()).replace(".", "")[5:10]
 
-# 登录并获取token
+# 登录一级用户并获取token
 def get_token(username="zhaohexiang1", password="72732414d750775c1eb0b5b915b3561b"):
     url = "http://10.23.190.107/zhgd_mms/user/login"
     headers = {
@@ -42,6 +42,36 @@ def get_token(username="zhaohexiang1", password="72732414d750775c1eb0b5b915b3561
 if __name__ == "__main__":
     return_token = get_token()
     print(return_token)
+
+# 登录二级用户并获取token
+def get_token1(username="ceshi_admin", password="72732414d750775c1eb0b5b915b3561b"):
+    url = "http://10.23.190.107/zhgd_mms/user/login"
+    headers = {
+        "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8",
+        "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
+    }
+    body = {
+        "_sign": 'bd49d5b78c71d2268c181e9c70ba9233',
+        "_timestamp": '1596009876000',
+        "loginName": username,
+        "password": password,
+        "terminal":'pc',
+        "token": '',
+        "v": ''
+    }
+    r = requests.post(url, headers=headers, data=body)
+    try:
+        # 获取token的方法：["一级"]["二级"]["..."]
+        return_token = r.json()["results"]["data"]["token"]
+    except:
+        print("没取到token \n %s" % r.text)
+        return_token = ''
+    return return_token
+
+if __name__ == "__main__":
+    return_token = get_token1()
+    print(return_token)
+
 
 
 #     res = requests.post(url, headers=headers, data=body)
